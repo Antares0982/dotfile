1c74d837e5fd578333d69c6a7e7e3949daa105ed
aLyrae0 <alyr@alyr.dev>
2026-05-26 23:26:17 +0800

    rpi/xray: subscription updater timer, shared group, symlink-based switching (#9)
    * rpi/xray: subscription updater timer, shared group, symlink-based switching
    
    Add xray-sub systemd service + daily timer using rules-dat-xray-flake.
    Create xray shared group (antares:hermes) for /var/lib/xray/subscriptions/.
    Migrate /var/lib/xray/config.json to symlink for runtime switching.
    Template based on current routing/inbounds with placeholder outbounds[0].
    Sudo: allow hermes to restart xray and xray-sub without password.
    
    * rpi/xray-sub: symlink agenix secret, cleanup via trap
    
    * rpi/xray: move template to agenix secret, cleanup on exit
    
    ---------
    
    Co-authored-by: aLyrae0 <lyrae@antares.dev>

--- common/sudo.nix
+++ common/sudo.nix
@@ -8,12 +8,17 @@
   security = {
     sudo = {
       wheelNeedsPassword = false;
-    }
-    // (lib.optionalAttrs currentDevice.useProxy {
-      extraConfig = ''
-        Defaults env_keep += "http_proxy https_proxy"
-      '';
-    });
+      extraConfig = lib.concatStringsSep "\n" (
+        lib.optional currentDevice.useProxy ''
+          Defaults env_keep += "http_proxy https_proxy"
+        ''
+        ++ lib.optional currentDevice.rpi ''
+          hermes ALL=(root) NOPASSWD: /run/current-system/sw/bin/systemctl restart xray
+          hermes ALL=(root) NOPASSWD: /run/current-system/sw/bin/systemctl restart xray-sub
+          hermes ALL=(root) NOPASSWD: /run/current-system/sw/bin/systemctl start xray-sub
+        ''
+      );
+    };
     pam.services.su.requireWheel = true;
   };
 }
