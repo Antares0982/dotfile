c971d6bece2680338529e3e089d2547c30258f41
antares <antares@alyr.dev>
2026-05-31 17:53:14 +0800

    Refactor

--- pc/display.nix
+++ pc/display.nix
@@ -1,30 +1,33 @@
 { config, pkgs, ... }:
 {
-  programs.niri.enable = true;
-  services.gnome.gcr-ssh-agent.enable = false;
-
-  programs.kdeconnect = {
-    enable = true;
-    package = pkgs.kdePackages.kdeconnect-kde;
-  };
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri-session -r";
-        user = "greeter";
-      };
+  programs = {
+    niri.enable = true;
+    xwayland.enable = true;
+    kdeconnect = {
+      enable = true;
+      package = pkgs.kdePackages.kdeconnect-kde;
     };
   };
 
-  services.gvfs.enable = true;
-  environment.systemPackages = with pkgs; [
-    gvfs
-    xwayland-satellite
-  ];
+  services = {
+    gnome.gcr-ssh-agent.enable = false;
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri-session -r";
+          user = "greeter";
+        };
+      };
+    };
+    gvfs.enable = true; # for kdeconnect
+  };
 
-  programs.xwayland.enable = true;
-
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment = {
+    systemPackages = with pkgs; [
+      gvfs
+      xwayland-satellite
+    ];
+    sessionVariables.NIXOS_OZONE_WL = "1";
+  };
 }
