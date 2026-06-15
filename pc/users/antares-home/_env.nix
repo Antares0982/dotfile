c33e7bbb806e0a1309acb0a8cb7edf382fcc64b3
antares <antares@alyr.dev>
2026-06-15 23:30:42 +0800

    Update

--- pc/users/antares-home/_env.nix
+++ pc/users/antares-home/_env.nix
@@ -124,15 +124,6 @@ rec {
     }
 
     zc() { _zz c "$@"; }
-
-    export ANTHROPIC_BASE_URL=https://api.deepseek.com/anthropic
-    export ANTHROPIC_AUTH_TOKEN=$(cat /run/agenix/deepseekAPIKey)
-    export ANTHROPIC_MODEL=deepseek-v4-pro[1m]
-    export ANTHROPIC_DEFAULT_OPUS_MODEL=deepseek-v4-pro[1m]
-    export ANTHROPIC_DEFAULT_SONNET_MODEL=deepseek-v4-pro[1m]
-    export ANTHROPIC_DEFAULT_HAIKU_MODEL=deepseek-v4-flash
-    export CLAUDE_CODE_SUBAGENT_MODEL=deepseek-v4-flash
-    export CLAUDE_CODE_EFFORT_LEVEL=max
   ''
   + lib.concatStrings (lib.mapAttrsToList genNixFunc nixFuncAliases);
 }
