fe55415d89e3169c6a13b0a41020f8401e80c5af
antares <antares@alyr.dev>
2026-09-09 00:03:30 +0800

    Try

--- common/nix.nix
+++ common/nix.nix
@@ -8,7 +8,7 @@
 {
   nix = {
     settings = {
-      experimental-features = "nix-command flakes";
+      experimental-features = [ "nix-command flakes" ];
       trusted-users = [
         "root"
         "antares"
