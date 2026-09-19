36425c7a7f0cf7ccab54692a93170c4190e0bdba
antares <antares@alyr.dev>
2026-09-19 13:39:55 +0800

    clean up

--- server/hk/users/default.nix
+++ server/hk/users/default.nix
@@ -23,29 +23,18 @@ in
       inherit (userCommonSettings) hashedPasswordFile;
       linger = true;
     };
-    # openai = {
-    #   isNormalUser = true;
-    #   home = "/home/openai";
-    #   description = "openai session keeper";
-    #   useDefaultShell = true;
-    #   openssh.authorizedKeys.keys = [
-    #     userCommonSettings.commonUserAuthorizedKey
-    #   ];
-    #   inherit (userCommonSettings) hashedPasswordFile;
-    #   linger = true;
-    # };
+
     acme = {
       extraGroups = [ "nginx" ];
     };
   };
   # home manager
-  home-manager.useGlobalPkgs = true;
-  home-manager.users = {
-    alice = import ./alice;
-    # openai = import ./openai;
-  };
-  home-manager.backupFileExtension = "backup";
-  home-manager.extraSpecialArgs = {
-    inherit myXray antares-rpc-client;
+  home-manager = {
+    useGlobalPkgs = true;
+    users = {
+      alice = import ./alice;
+    };
+    backupFileExtension = "backup";
+    extraSpecialArgs = { inherit myXray antares-rpc-client; };
   };
 }
